4bf2531f733872d5692f63b607d331887a531b7c
Frank Zhao <bz2190@nyu.edu>
2023-11-30 22:00:37 -0500

    changed back

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,7 +3,6 @@ import Ticket from "../components/Ticket";
 import Navbar from "../components/Navbar";
 
 import { useState } from "react";
-import { useEffect } from "react";
 
 
 const Home = () => {
@@ -12,28 +11,6 @@ const Home = () => {
   const handleFlights = (data) => {
     setFlights(data);
   };
-  const getSpending = async () => {
-    const response = await fetch('http://localhost:4000/api/profile/get_spending', {
-        method: 'POST',
-        headers: {'Content-type':'application/json'},
-        body: JSON.stringify({
-            "email":"a@c.com",
-            "start_date":"2023-1-14 00:00:00",
-            "end_date":"2023-11-30 00:00:00"
-        })
-    })
-    const json = await response.json();
-    if (!response.ok){
-        console.log(json.error);
-    }
-    if (response.ok){
-        console.log(json);
-    }
-}
-
-useEffect(() =>{
-    getSpending();
-}, []);
 
     return ( 
         <>
